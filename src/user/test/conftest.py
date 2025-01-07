54220ca69a944bff9b8b1adc34c4700f5bf739ed
wkdsh21 <wkdsh21@github.com>
2025-01-07 19:06:02 +0900

    travel router test cdoe 수정

--- src/user/test/conftest.py
+++ src/user/test/conftest.py
@@ -4,9 +4,10 @@ from typing import AsyncGenerator, Generator
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
+from src.config import settings
 from src.config.orm import Base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:0000@localhost:5432/testdb"
+DATABASE_URL = settings.TEST_ASYNC_DATABASE_URL
 
 # 비동기 엔진 생성
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
